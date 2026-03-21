bc8dfc51e42fab62d849024930e22d0d85a82c36
Kavshick K S <suthakrushick@gmail.com>
2026-03-22 00:04:42 +0530

    Fix API routing - revert handler and add explicit rewrites

--- api/[...path].js
+++ api/[...path].js
@@ -1,14 +1,4 @@
 const app = require('../backend/app');
 
-module.exports = (req, res) => {
-  // Set CORS headers
-  if (req.method === 'OPTIONS') {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    return res.status(200).end();
-  }
-
-  // Handle the request with Express app
-  return app(req, res);
-};
+// Vercel serverless handler
+module.exports = app;
